20fcc2db1c39509fb93e883e5d523a4ff4886a88
Ragnazar <avdeyev.iv@gmail.com>
2022-08-05 15:49:46 +0300

    add_split_and_contains

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task2(task1());
         task3(task1());
     }
+
     public static String task1() {
         String lastName = "Ivanov";
         String middleName = "Ivanovich";
@@ -13,13 +14,28 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         return fullName;
     }
+
     private static void task2(String fullName) {
         System.out.println("ФИО сотрудника — " + fullName);
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
+
     private static void task3(String fullName) {
         fullName = "Иванов Семён Семёнович";
+
+        //Простой replace()
         System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
+
+        //Использованы split(), contains()
+        String[] splitFullName = fullName.split("");
+        for (int i = 0; i < splitFullName.length; i++) {
+            if (splitFullName[i].contains("ё")) {
+                splitFullName[i] = "е";
+            }
+        }
+        String splitToString = String.join("", splitFullName);
+        System.out.println("Данные ФИО сотрудника — " + splitToString);
+
     }
 
 }
